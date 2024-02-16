738085714089cd465a9438d61417e06a0e3dc6fe
amitj999998 <160257623+amitj999998@users.noreply.github.com>
2024-02-16 12:03:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
+  st.write(submitted)
   st.write(openai_api_key)
   if not openai_api_key.startswith('hf-'):
     st.write("error")
